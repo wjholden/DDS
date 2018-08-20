1d4136b11284bdebf99d2a36441f31ddcf9ad6e3
William John Holden <wjholden@gmail.com>
2018-08-20 11:04:10 +0000

    Minor updates

--- build.sh
+++ build.sh
@@ -1,14 +1,9 @@
-T=`mktemp -d`
-chmod a+r $T
-chmod a+x $T
-echo "Using temporary directory $T"
-
 docker network create --driver bridge wjholden-net
 
 docker stop www
 docker container rm www
 docker build --tag wjholden-www www/
-docker run --detach --publish 80:80 --publish 443:443 --volume `pwd`/www/src:/var/www/html --name www --network wjholden-net wjholden-www
+docker run --detach 443:443 --name www --network wjholden-net wjholden-www
 
 docker stop git
 docker container rm git
